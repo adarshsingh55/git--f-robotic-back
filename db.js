2478c1d525ebe2eb9121320d535d9bcc92d59c33
Adarsh singh <adarshsingh345125@gmail.com>
2022-08-12 15:12:03 +0530

    hello this is mongo

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 
 const mongoose = require('mongoose');
-const mongoUri="mongodb://localhost:27017/gitrobotic"
+// const mongoUri="mongodb://localhost:27017/gitrobotic"
+const mongoUri="mongodb+srv://vaibhav:a12345d@vaibhav.kbcq1.mongodb.net/gitrobotic?retryWrites=true&w=majority"
 
 
 const connectToMongo =()=>{ 
     mongoose.connect(mongoUri ,()=>{
-        console.log('connnect to mongo successfully'); })
+        console.log('connnect to mongo successfully');
+     })
 }
 module.exports=connectToMongo
